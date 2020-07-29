5e2b9da8eed5986e859f6dbda31f58afd8f0f5a0
Kevin Medina <kmedinakm@hotmail.com>
2020-07-28 21:09:20 -0400

    install socket.io

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const http = require("http");
 const cookieParser = require("cookie-parser");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 const path = require("path");
+const socketio = require();
 const cors = require("cors");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const app = express();
 
@@ -23,6 +26,9 @@ require("./configs/session.config")(app);
 require("./configs/passport/passport.config.js")(app);
 require("./configs/db.config");
 
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.use(
   cors({
     origin: true,
@@ -30,19 +36,59 @@ app.use(
   })
 );
 
-app.use(
-  session({
-    secret: "Lets go champ",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 // app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log(`Server is running on port: ${PORT}`));
+server.listen(PORT, console.log(`Server is running on port: ${PORT}`));
 
 app.use("/", require("./routes/index"));
 app.use("/", require("./routes/auth.routes"));
