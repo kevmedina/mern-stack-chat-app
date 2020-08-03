795c55871445a1e0e3fe196ccde967ab76f3c1b5
Kevin Medina <kmedinakm@hotmail.com>
2020-08-03 18:59:34 -0400

    add server side connectionfor socketio

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
 const http = require("http");
+const bodyParser = require("body-parser");
+const socketio = require("socket.io");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const path = require("path");
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const server = http.createServer(app);
+const io = socketio(server);
+
 // make sure express- session is used before the passport
 require("./configs/session.config")(app);
 require("./configs/passport/passport.config.js")(app);
@@ -31,12 +34,60 @@ app.use(
   })
 );
 
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/index"));
 app.use("/", require("./routes/auth.routes"));
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log(`Server is running on port: ${PORT}`));
+server.listen(PORT, console.log(`Server is running on port: ${PORT}`));
 
 module.exports = app;
